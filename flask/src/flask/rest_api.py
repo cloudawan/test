c40b51ba63a5efd91fe3d82933e84fe47340e3df
cloudawan <staff@cloudawan.com>
2015-08-05 15:36:18 -0700

    hello 2

--- flask/src/flask/rest_api.py
+++ flask/src/flask/rest_api.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     try:
-        return '{"Flask":"hello"}'
+        return '{"Flask":"hello 2"}'
     except Exception as e:
         return "Error"
 
