b5c57c759ae600b49a54abc1cf224cc03647a0cb
cloudawan <staff@cloudawan.com>
2015-08-05 17:26:23 -0700

    Updated msg

--- flask/src/flask/rest_api.py
+++ flask/src/flask/rest_api.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     try:
-        return '{"Flask":"hello world! 8/15/2015 4:05 PM"}'
+        return '{"Flask":"hello world! Frank update 8/15/2015 5:25 PM"}'
     except Exception as e:
         return "Error"
 
