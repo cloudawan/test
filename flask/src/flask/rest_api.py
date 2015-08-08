f9c05d168d6d6a3ed47a933a753b18b2ded4b621
cloudawan <staff@cloudawan.com>
2015-08-08 15:02:12 -0700

    demo

--- flask/src/flask/rest_api.py
+++ flask/src/flask/rest_api.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     try:
-        return '{"Flask":"hello world! VERSION:11 8/16/2015 1:35 PM"}'
+        return '{"Flask":"hello world Manu! VERSION:11 8/8/2015 2:35 PM"}'
     except Exception as e:
         return "Error"
 
