47b9f57c136d85914b68e6a65c86c2a222afcce6
edcyc <edcyc.tw@gmail.com>
2016-05-02 18:54:03 +0800

    Change

--- flask/src/flask/rest_api.py
+++ flask/src/flask/rest_api.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     try:
-        return '{"Flask":"hello world!"}' 
+        return '{"Flask":"hello world! v2"}' 
     except Exception as e:
         return "Error"
 
