b2a5f054f114aa102155cba5d2ee390df8da8f30
jjokom <jjokom@users.noreply.github.com>
2016-06-03 11:52:57 -0700

    Update rest_api.py

--- flask/src/flask/rest_api.py
+++ flask/src/flask/rest_api.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     try:
-        return '{"Flask":"hello world! Green"}' 
+        return '{"Flask":"hello world! George"}' 
     except Exception as e:
         return "Error"
 
